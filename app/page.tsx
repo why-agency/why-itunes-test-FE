cd4251874cea85c7b137b90e550eaf9f9b5319e3
Manuel Lippe <mail@lippe-mann.de>
2026-02-05 15:48:08 +0100

    feat(page): make more fancy (hover states)

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,9 @@ export default async function Home() {
   return (
     <div className='font-sans w-full'>
       <main className='grow flex flex-col  size-full p-6'>
-        <div className='mb-10 pb-10 border-b flex items-center flex-col gap-10'>
+        <div className='mb-15 pb-15 border-b flex items-center flex-col gap-5'>
           <h1 className='text-4xl font-bolder'>Welcome to &WhyTunes</h1>
+
           <SearchField name='search' className='flex flex-col items-center'>
             <Label className='w-fit'>Search to get stated</Label>
             <SearchField.Group>
@@ -41,22 +42,28 @@ export default async function Home() {
             </SearchField.Group>
           </SearchField>
         </div>
-        <div className='flex flex-col gap-10 mt-10'>
-          <section className='flex flex-col gap-10'>
+        <div className='flex flex-col gap-16 '>
+          <section className='flex flex-col gap-5'>
             <h2 className='text-3xl'>Artists</h2>
-            <article className='flex flex-col gap-6'>
+            <article className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
               {artists && artists.length > 0 ? (
                 artists.map((artist, i) => (
                   <Link
                     key={`display-artist-info-${i}`}
                     href={artist.artistLinkUrl}
-                    className='group'
+                    className='group relative'
+                    target='_blank'
                   >
-                    <Card className='group-hover:-translate-y-1 transition-all duration-500 group-hover:shadow-xl'>
+                    <Card className='group-hover:-translate-y-0.5 transition-all duration-500 group-hover:shadow-lg group-hover:bg-black/5'>
                       <Card.Header>
-                        <Card.Title>{artist.artistName}</Card.Title>
+                        <Card.Title className='font-bold'>
+                          {artist.artistName}
+                        </Card.Title>
                       </Card.Header>
                     </Card>
+                    <p className='absolute bottom-1/2 translate-y-1/2 right-4 bg-white/60 px-3 py-2 text-xs rounded-full backdrop-blur-3xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                      View Artist on iTunes
+                    </p>
                   </Link>
                 ))
               ) : (
@@ -64,26 +71,32 @@ export default async function Home() {
               )}
             </article>
           </section>
-          <section className='flex flex-col gap-10'>
+          <section className='flex flex-col gap-5'>
             <h2 className='text-3xl'>Albums</h2>
             <article className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-6'>
-              {albums.map((album, i) => (
-                <AlbumDisplay key={`display-album-info-${i}`} album={album} />
-              ))}
+              {albums && albums.length > 0 ? (
+                albums.map((album, i) => (
+                  <AlbumDisplay key={`display-album-info-${i}`} album={album} />
+                ))
+              ) : (
+                <div>Sorry, no results...</div>
+              )}
             </article>
           </section>
 
-          <section className='flex flex-col gap-10'>
+          <section className='flex flex-col gap-5'>
             <h2 className='text-3xl'>Songs</h2>
             <article className='flex flex-col gap-6'>
-              {tracks.map((track, i) => (
-                <Link
-                  key={`display-artist-info-${i}`}
-                  href={track.trackViewUrl}
-                >
-                  <Card>
-                    <Card.Header>
-                      <Card.Title>
+              {tracks && tracks.length > 0 ? (
+                tracks.map((track, i) => (
+                  <Link
+                    key={`display-artist-info-${i}`}
+                    href={track.trackViewUrl}
+                    className='group'
+                    target='_blank'
+                  >
+                    <Card className='group-hover:-translate-y-0.5 transition-all duration-500 group-hover:shadow-lg group-hover:bg-black/5 relative'>
+                      <Card.Content>
                         <span className='text-muted'>
                           <span className='text-foreground font-bold'>
                             {track.trackName}
@@ -97,11 +110,16 @@ export default async function Home() {
                             {track.collectionName}
                           </span>
                         </span>
-                      </Card.Title>
-                    </Card.Header>
-                  </Card>
-                </Link>
-              ))}
+                        <p className='absolute bottom-1/2 translate-y-1/2 right-4 bg-white/60 px-3 py-2 text-xs rounded-full backdrop-blur-3xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                          View Song on iTunes
+                        </p>
+                      </Card.Content>
+                    </Card>
+                  </Link>
+                ))
+              ) : (
+                <div>Sorry, no results...</div>
+              )}
             </article>
           </section>
         </div>
--- components/AlbumDisplay.tsx
+++ components/AlbumDisplay.tsx
@@ -1,38 +1,44 @@
 import { ItunesAlbum } from '@/lib/types';
 import { Card } from '@heroui/react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function AlbumDisplay({ album }: { album: ItunesAlbum }) {
   return (
-    <Card className='relative shadow-xl'>
-      <Image
-        src={album.artworkUrl100!}
-        width={100}
-        height={100}
-        alt={`Album Arwork of ${album.collectionName}`}
-        className='absolute blur-lg inset-0 size-full  obejct cover opacity-40 z-0'
-      />
-      <Card.Content className='flex flex-col items-center z-10 relative'>
+    <Link href={album.collectionViewUrl} target='_blank'>
+      <Card className='relative shadow-xl group h-full'>
         <Image
           src={album.artworkUrl100!}
           width={100}
           height={100}
           alt={`Album Arwork of ${album.collectionName}`}
-          className='relative z-10 rounded-sm'
+          className='absolute blur-lg inset-0 size-full  obejct cover opacity-40 z-0 transition-all duration-700 ease group-hover:scale-125'
         />
-        <div className='w-full flex flex-col justify-center mt-2 text-foreground'>
-          <span className='text-center  w-full text-sm font-medium'>
-            {album.releaseDate && new Date(album.releaseDate!).getFullYear()}
-          </span>
-          <span className='text-center text-lg w-full font-bold line-clamp-2'>
-            {album.collectionName}
-          </span>
-          <span className='text-center text-sm w-full'>
-            {album.trackCount}{' '}
-            {album.trackCount && album.trackCount > 1 ? 'Tracks' : 'Track'}
-          </span>
-        </div>
-      </Card.Content>
-    </Card>
+        <Card.Content className='flex flex-col items-center z-10 relative'>
+          <Image
+            src={album.artworkUrl100!}
+            width={100}
+            height={100}
+            alt={`Album Arwork of ${album.collectionName}`}
+            className='relative z-10 rounded-sm'
+          />
+          <div className='w-full flex flex-col justify-center mt-2 text-foreground'>
+            <span className='text-center  w-full text-sm font-medium'>
+              {album.releaseDate && new Date(album.releaseDate!).getFullYear()}
+            </span>
+            <span className='text-center text-lg w-full font-bold line-clamp-2'>
+              {album.collectionName}
+            </span>
+            <span className='text-center text-sm w-full'>
+              {album.trackCount}{' '}
+              {album.trackCount && album.trackCount > 1 ? 'Tracks' : 'Track'}
+            </span>
+          </div>
+        </Card.Content>
+        <p className='absolute bottom-2 -translate-x-1/2 left-1/2 bg-white/60 px-3 py-2 text-xs rounded-full backdrop-blur-3xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20 text-center min-w-32 '>
+          View Album on iTunes
+        </p>
+      </Card>
+    </Link>
   );
 }
