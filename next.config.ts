e608bc4d42607fa83a78f11a364be509899de9d0
Manuel Lippe <mail@lippe-mann.de>
2026-02-05 14:48:53 +0100

    fix(project): allow external image loading with nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://is1-ssl.mzstatic.com/**')],
+  },
 };
 
 export default nextConfig;
