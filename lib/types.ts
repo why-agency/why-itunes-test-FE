2950c97d498d5e58daaf2f407151ad207079d85d
Manuel Lippe <mail@lippe-mann.de>
2026-02-05 14:48:31 +0100

    feat(api-request): add request and first content to page, add types

--- lib/types.ts
+++ lib/types.ts
@@ -1,1 +1,88 @@
-// Here be type definitions
+// types/itunes.ts
+
+export type WrapperType = 'artist' | 'collection' | 'track';
+export type Explicitness = 'explicit' | 'cleaned' | 'notExplicit';
+
+export interface ItunesBase {
+  wrapperType: WrapperType;
+  artistId?: number;
+  artistName?: string;
+  artistViewUrl?: string;
+  primaryGenreName?: string;
+  country?: string;
+  currency?: string;
+}
+
+export interface ItunesArtist extends ItunesBase {
+  wrapperType: 'artist';
+  artistType: 'Artist';
+  artistId: number;
+  artistName: string;
+  artistLinkUrl: string;
+  amgArtistId?: number;
+  primaryGenreId?: number;
+}
+
+export interface ItunesAlbum extends ItunesBase {
+  wrapperType: 'collection';
+  collectionType: 'Album' | string;
+
+  artistId: number;
+  collectionId: number;
+  artistName: string;
+
+  collectionName: string;
+  collectionCensoredName?: string;
+
+  collectionViewUrl: string;
+  artworkUrl60?: string;
+  artworkUrl100?: string;
+
+  collectionPrice?: number;
+  collectionExplicitness?: Explicitness;
+  contentAdvisoryRating?: string;
+
+  trackCount?: number;
+  copyright?: string;
+  releaseDate?: string;
+}
+
+export interface ItunesTrack extends ItunesBase {
+  wrapperType: 'track';
+  kind: 'song' | string;
+
+  artistId: number;
+  collectionId: number;
+  trackId: number;
+
+  artistName: string;
+  collectionName: string;
+  trackName: string;
+
+  collectionViewUrl: string;
+  trackViewUrl: string;
+  previewUrl?: string;
+
+  artworkUrl30?: string;
+  artworkUrl60?: string;
+  artworkUrl100?: string;
+
+  trackPrice?: number;
+  collectionPrice?: number;
+
+  collectionExplicitness?: Explicitness;
+  trackExplicitness?: Explicitness;
+
+  discNumber?: number;
+  trackNumber?: number;
+  trackTimeMillis?: number;
+
+  isStreamable?: boolean;
+}
+
+export type ItunesResult = ItunesArtist | ItunesAlbum | ItunesTrack;
+
+export interface ItunesSearchResponse {
+  resultCount: number;
+  results: ItunesResult[];
+}
