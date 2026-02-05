5033a5efb671be5f66e36296d8ee6aaa7025d124
Manuel Lippe <mail@lippe-mann.de>
2026-02-05 15:29:56 +0100

    feat(page): add more content

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {
   ItunesTrack,
 } from '@/lib/types';
 import { isAlbum, isArtist, isTrack } from '@/lib/utils';
-import { Card } from '@heroui/react';
+import { Card, Label, SearchField } from '@heroui/react';
 import Link from 'next/link';
 
 async function getItunesResults(term: string) {
@@ -28,31 +28,79 @@ export default async function Home() {
   console.log(artists);
 
   return (
-    <div className='font-sans h-dvh w-full'>
+    <div className='font-sans w-full'>
       <main className='grow flex flex-col  size-full p-6'>
-        <h1 className='text-4xl font-bolder'>Welcome to &WhyTunes</h1>
+        <div className='mb-10 pb-10 border-b flex items-center flex-col gap-10'>
+          <h1 className='text-4xl font-bolder'>Welcome to &WhyTunes</h1>
+          <SearchField name='search' className='flex flex-col items-center'>
+            <Label className='w-fit'>Search to get stated</Label>
+            <SearchField.Group>
+              <SearchField.SearchIcon />
+              <SearchField.Input className='' placeholder='Search...' />
+              <SearchField.ClearButton />
+            </SearchField.Group>
+          </SearchField>
+        </div>
         <div className='flex flex-col gap-10 mt-10'>
           <section className='flex flex-col gap-10'>
             <h2 className='text-3xl'>Artists</h2>
-            <article className='grid grid-cols-4 gap-6'>
-              {artists.map((artist, i) => (
-                <Card key={`display-artist-info-${i}`}>
-                  <Card.Header>
-                    <Card.Title>
-                      <Link href={artist.artistLinkUrl}>
-                        {artist.artistName}
-                      </Link>
-                    </Card.Title>
-                  </Card.Header>
-                </Card>
-              ))}
+            <article className='flex flex-col gap-6'>
+              {artists && artists.length > 0 ? (
+                artists.map((artist, i) => (
+                  <Link
+                    key={`display-artist-info-${i}`}
+                    href={artist.artistLinkUrl}
+                    className='group'
+                  >
+                    <Card className='group-hover:-translate-y-1 transition-all duration-500 group-hover:shadow-xl'>
+                      <Card.Header>
+                        <Card.Title>{artist.artistName}</Card.Title>
+                      </Card.Header>
+                    </Card>
+                  </Link>
+                ))
+              ) : (
+                <div>Sorry, no results...</div>
+              )}
             </article>
           </section>
           <section className='flex flex-col gap-10'>
             <h2 className='text-3xl'>Albums</h2>
-            <article className='grid grid-cols-4 gap-6'>
+            <article className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-6'>
               {albums.map((album, i) => (
                 <AlbumDisplay key={`display-album-info-${i}`} album={album} />
+              ))}
+            </article>
+          </section>
+
+          <section className='flex flex-col gap-10'>
+            <h2 className='text-3xl'>Songs</h2>
+            <article className='flex flex-col gap-6'>
+              {tracks.map((track, i) => (
+                <Link
+                  key={`display-artist-info-${i}`}
+                  href={track.trackViewUrl}
+                >
+                  <Card>
+                    <Card.Header>
+                      <Card.Title>
+                        <span className='text-muted'>
+                          <span className='text-foreground font-bold'>
+                            {track.trackName}
+                          </span>{' '}
+                          by{' '}
+                          <span className='text-foreground font-bold'>
+                            {track.artistName}
+                          </span>{' '}
+                          on Album{' '}
+                          <span className='text-foreground font-bold'>
+                            {track.collectionName}
+                          </span>
+                        </span>
+                      </Card.Title>
+                    </Card.Header>
+                  </Card>
+                </Link>
               ))}
             </article>
           </section>
