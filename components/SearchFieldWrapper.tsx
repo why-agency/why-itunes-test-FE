b270f7d78788518a87ae50430f6fc0f2c3f132c0
Manuel Lippe <mail@lippe-mann.de>
2026-02-05 16:00:57 +0100

    feat(components): add search field wrapper

---
+++ components/SearchFieldWrapper.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Button, SearchField } from '@heroui/react';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export function SearchFieldWrapper({
+  defaultValue = '',
+}: {
+  defaultValue?: string;
+}) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [value, setValue] = useState(defaultValue);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (value) {
+      params.set('q', value);
+    } else {
+      params.delete('q');
+    }
+
+    router.push(`/?${params.toString()}`);
+  }
+
+  return (
+    <form onSubmit={onSubmit} className='flex gap-4 items-end'>
+      <SearchField name='search'>
+        <SearchField.Group>
+          <SearchField.SearchIcon />
+          <SearchField.Input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className=''
+            placeholder='Search...'
+          />
+          <SearchField.ClearButton />
+        </SearchField.Group>
+      </SearchField>
+      <Button type='submit' className={'text-white'}>
+        Search
+      </Button>
+    </form>
+  );
+}
